a5edafe8cf81359ff9f087daadc7ee9ef6afb37f
ssveg2 <38942874+ssveg2@users.noreply.github.com>
2018-07-21 18:39:55 +0800

    Dict  key-values usage

---
+++ dict_test1.py
@@ -0,0 +1,14 @@
+birthday = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4'}
+while True:
+    print('Enter a name: (blank to quit)')
+    name = input()
+    if name == ' ':
+        break
+    if name in birthday:
+        print(birthday[name] + ' is the birthday of ' + name)
+    else:
+        print('I do not have birthday information for ' + name)
+        print('What is their birthday?')
+        bday = input()
+        birthday[name] = bday
+        print('Birthday database updated.')
