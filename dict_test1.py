4e826a5c35a4af79a5974acd8deb6f7870db9fa2
ssveg2 <38942874+ssveg2@users.noreply.github.com>
2018-07-22 00:42:44 +0000

    重复
    要新建.py文件再上传！

--- dict_test1.py
+++
@@ -1,6 +0,0 @@
-message = 'It was a bright cold day in April, and the clocks were striking thirteen'   #定义字符串，即单个字符的列表
-count = {}                                                                             #定义空字典
-for character in message:                                                              #遍历字符串
-    count.setdefault(character, 0)                                                     #初始化character键值
-    count[character] = count[character] + 1                                            #更新character键值
-print(count)
